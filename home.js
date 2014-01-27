31db7dfb2d11acc190126157e8a70cac69626862
Robert j Chatfield <robert.chatfield@griffithuni.edu.au>
2014-01-27 23:51:38 +1000

    Include 2014 details

--- home.js
+++ home.js
@@ -1,17 +1,13 @@
 var INIT_DELAY = 1200, SHOW_DELAY = 100,
     elCount, thisEl = 0, nextEl = -1,
-    smgroup,
-    greatSayings = [
-      "Code the change you wish to see in the world.",
-      "What you do is important, because you are important.",
-      "I shall pass this way, but only once. <br/>All the good that I can do, I must do, without any hesitation. <br/>For I shall pass this way but only once."
-    ];
+    smgroup;
 
 function loadPage() {
   smgroup = document.getElementById('socialmedias').children;
   elCount = Math.floor(smgroup.length / 2);
+  showDiv('tagline');
   setTimeout(function(){showel();}, INIT_DELAY);
-  printSaying();
+  setTimeout(function(){showDiv('footer');}, INIT_DELAY*2);
 }
 
 function showel(el) {
@@ -29,14 +25,13 @@ function showel(el) {
   }
 }
 
-function printSaying() {
-  var taglineEl = document.getElementById('tagline');
-  taglineEl.innerHTML = greatSayings[Math.floor(Math.random() * greatSayings.length)];
+function showDiv(d) {
+  var taglineEl = document.getElementById(d);
   taglineEl.classList.remove('hide');
 }
 
 function realName() {
   var h1 = document.getElementsByTagName('h1')[0];
-  if (h1.innerHTML[0] == 'R') {h1.innerHTML = "Smiley<br/>Rob";}
-  else {h1.innerHTML = "Robert<br/>Chatfield";}
+  if (h1.innerHTML[0] == 'R') {h1.innerHTML = "Smiley Rob";}
+  else {h1.innerHTML = "Robert J Chatfield";}
 }
