8a7373ef99ae27b76821a73141809457859cdfa9
Robert j Chatfield <robert.chatfield@griffithuni.edu.au>
2014-03-21 18:17:17 +1000

    Implement Amanda's Feedback

--- home.js
+++ home.js
@@ -3,35 +3,35 @@ var INIT_DELAY = 1200, SHOW_DELAY = 100,
     smgroup;
 
 function loadPage() {
-  smgroup = document.getElementById('socialmedias').children;
-  elCount = Math.floor(smgroup.length / 2);
-  showDiv('tagline');
-  setTimeout(function(){showel();}, INIT_DELAY);
-  setTimeout(function(){showDiv('footer');}, INIT_DELAY*2);
+    smgroup = document.getElementById('socialmedias').children;
+    elCount = Math.floor(smgroup.length / 2);
+    showDiv('textBlock1');
+    setTimeout(function(){showel();}, INIT_DELAY);
+    setTimeout(function(){showDiv('textBlock2');}, INIT_DELAY*2);
 }
 
 function showel(el) {
-  if (elCount >= 0) {
-    if (nextEl == -1) {
-      thisEl = elCount--;
-      nextEl = smgroup.length - thisEl;
-    } else {
-      thisEl = nextEl;
-      nextEl = -1;
+    if (elCount >= 0) {
+        if (nextEl == -1) {
+            thisEl = elCount--;
+            nextEl = smgroup.length - thisEl;
+        } else {
+            thisEl = nextEl;
+            nextEl = -1;
+        }
+        console.log('1: ' + thisEl + ', 2: ' + nextEl);
+        smgroup[thisEl].classList.remove('hide');
+        setTimeout(function(){showel();}, SHOW_DELAY);
     }
-    console.log('1: ' + thisEl + ', 2: ' + nextEl);
-    smgroup[thisEl].classList.remove('hide');
-    setTimeout(function(){showel();}, SHOW_DELAY);
-  }
 }
 
 function showDiv(d) {
-  var taglineEl = document.getElementById(d);
-  taglineEl.classList.remove('hide');
+    var taglineEl = document.getElementById(d);
+    taglineEl.classList.remove('hide');
 }
 
 function realName() {
-  var h1 = document.getElementsByTagName('h1')[0];
-  if (h1.innerHTML[0] == 'R') {h1.innerHTML = "Smiley Rob";}
-  else {h1.innerHTML = "Robert J Chatfield";}
+    var h1 = document.getElementsByTagName('h1')[0];
+    if (h1.innerHTML[0] == 'R') {h1.innerHTML = "Smiley Rob";}
+    else {h1.innerHTML = "Robert J Chatfield";}
 }
